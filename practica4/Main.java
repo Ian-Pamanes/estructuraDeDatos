1a3c865940d7032a30619bdc675d1fdfadf6d0c5
IAN-PC\Ian <A01373302@itesm.mx>
2017-10-05 11:27:00 -0500

    no message

--- practica4/Main.java
+++ practica4/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class Main {
 	
-	public static int linearSearch(int[] value, int x) {
+	public static int linearSearch(int value[], int x) {
 		for(int i = 0; i == value.length - 1; i++) {
 			if (value[i] == x) return i;
 			if (value[i] == x) return -1;
@@ -33,10 +33,32 @@ public class Main {
 			
 	}
 	
+	public static void selectionSort(int values[]) {
+		int smallest;
+		int smallestIndex;
+		for(int i = 0; i < values.length; i++) {
+			smallest = values[i];
+			smallestIndex = i;
+			for(int j = i; j < values.length; j++) {
+				if(values[j] < smallest) {
+					smallest = values[j];
+					smallestIndex = j;
+				
+				}
+			}	
+			values[smallestIndex] = values[i];
+			values[i] = smallest;
+					
+					
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		int[] values = {5,2,7,43,9,23,6,86,4,1};
+		int[] values2 = {5,2,7,43,9,23,6,86,4,1};
 		insertionSort(values);
+		selectionSort(values2);
 		System.out.println(Arrays.toString(values));
 		
 
